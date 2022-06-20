0fb449eb96f9707b15744b5a4d9204330d51a18f
Fahd Mannaa <egypingblog@gmail.com>
2022-06-20 21:51:45 +0300

    22 lecture done create user

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/user/UserResource.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -29,6 +31,11 @@ public class UserResource {
 		
 	}
 	
+	// create new user using POST
+	@PostMapping("/users")
+	public void createUser(@RequestBody User user) {
+		User SavedUser = service.save(user);
+	}
 	
 	
 	
